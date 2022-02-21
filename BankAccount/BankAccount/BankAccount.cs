bd16c7a231d7e50f37dbfa5293c909c9ca336cc2
JHON ALEJANDRO GARCIA ECHAVARRIA <jhongarcia288842@correo.itm.edu.co>
2022-02-21 16:50:45 -0500

    code complete but with any errors due to constructor and methods

---
+++ BankAccount/BankAccount/BankAccount.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountE
+{
+    public class CuentaBancaria
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public string NIT { get; set; }
+        public double Saldo { get; set; }
+
+        public CuentaBancaria(string nombrePa, string apellidoPa, string direccionPa, string NITPa, double saldoPa)
+        {
+            Nombre = nombrePa;
+            Apellido = apellidoPa;
+            Direccion = direccionPa;
+            NIT = NITPa;
+            Saldo = saldoPa;
+        }
+        public double Deposito(double montoPa)
+        {
+            Saldo = Saldo+ montoPa; 
+            return Saldo;
+        }
+        public double Retiro(double montoPa)
+        {
+            if (Saldo > montoPa)
+            {
+                Saldo = Saldo - montoPa;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente.");
+            }
+            return Saldo;
+        }
+        public void ConsultaSaldo()
+        {
+            Console.WriteLine($"Saldo:{Saldo}", 18);
+        }
+        public override string ToString()
+        {
+            return ($"\nNombre: {Nombre} " +
+                      $"\nApellido: {Apellido}" +
+                      $"\nDireccion: {Direccion}" +
+                      $"\nNIT: {NIT}" +
+                      $"\nSaldo: {Saldo}");
+        }
+    }
+}
---
+++ BankAccount/BankAccount/Program.cs
@@ -0,0 +1,59 @@
+﻿using BankAccountE;
+/*Variables*/
+int opcion = 0;
+double montoPa;
+/*Codigo*/
+Console.WriteLine("Bienvenido señor usuario.");
+Console.WriteLine("===============");
+
+    Console.WriteLine("Ingrese sus datos de usuario: ");
+    Console.WriteLine("Nombre: ");
+    string nombrePa = Console.ReadLine();
+    Console.WriteLine("Apellido: ");
+    string apellidoPa = Console.ReadLine();
+    Console.WriteLine("Direccion: ");
+    string direccionPa = Console.ReadLine();
+    Console.WriteLine("NIT: ");
+    string NITPa = Console.ReadLine();
+    Console.WriteLine("Saldo: ");
+    double saldoPa = int.Parse(Console.ReadLine());
+
+    var usuario = new CuentaBancaria(nombrePa, apellidoPa, direccionPa, NITPa, saldoPa);
+
+do
+{
+    Console.WriteLine($"\n1. Deposito" +
+                            $"\n2. Retiro" +
+                            $"\n3. Consultar Saldo" +
+                            $"\n4. Mostrar informacion de la cuenta" +
+                            $"\n5. Salir");
+
+    opcion = int.Parse(Console.ReadLine());
+    switch (opcion)
+    {
+        case 1:
+            Console.WriteLine("Ingrese la cantidad a depositar: ");
+            montoPa = double.Parse(Console.ReadLine());
+            usuario.Deposito(montoPa);
+            break;
+        case 2:
+            Console.WriteLine("Ingrese la cantidad a retirar: ");
+            montoPa = double.Parse(Console.ReadLine());
+            usuario.Retiro(montoPa);
+            break;
+
+        case 3:
+            usuario.ConsultaSaldo();
+            break;
+        case 4:
+            Console.WriteLine(usuario);
+            break;
+        case 5:
+            Console.WriteLine("Saliendo");
+            break;
+        default:
+            Console.WriteLine("Opcion invalida, intente de nuevo.");
+            break;
+    }
+} while (opcion != 5 );
+
